ae48052726268c8adf136656d91306b448e1f3ef
Yuncong Chen <cyc3700@gmail.com>
2013-06-13 03:52:20 -0700

    initial commit comment

---
+++ src/CraigsCrawler/main.py
@@ -0,0 +1,69 @@
+'''
+Created on Jun 12, 2013
+
+@author: yuncong
+'''
+
+import xml.etree.ElementTree as ET
+import urllib
+import re
+import csv
+
+prefix = '{http://purl.org/rss/1.0/}'
+
+trim = ['ex','lx','si']
+level = ['sedan','coupe']
+years = [str(y) for y in range(1992,2014)]
+years_short = [y[-2:]for y in years]
+transm = ['automatic','manual']
+
+if __name__ == '__main__':
+    items = []
+    srch_year = '2006'
+    brand = 'honda'
+    model = 'civic'
+#     url = 'http://sandiego.craigslist.org/search/cta?query='+srch_year+'%20'+brand+'%20'+model+'&srchType=T&format=rss'
+    url = 'http://sandiego.craigslist.org/search/cta?query='+brand+'%20'+model+'&srchType=T&format=rss'
+    xml_str = urllib.urlopen(url).read()
+    root = ET.fromstring(xml_str)
+    for child in root.iter(prefix+'item'):
+        item = dict([('title',None),('link',None),('desc',None),('price',None),
+                     ('transm',None),('year',None),('level',None),('trim',None)])
+        for c in child:
+            if re.match('\{.*\}date',c.tag):
+                item['date'] = c.text
+            elif re.match('\{.*\}title',c.tag) :
+                item['title'] = c.text
+                title_lower = c.text.lower()
+                price = re.findall('\$[0-9]{4,5}',c.text)
+                item['price'] = price[0] if len(price)>0 else None
+            elif re.match('\{.*\}link',c.tag) :
+                item['link'] = c.text
+            elif re.match('\{.*\}description',c.tag):
+                item['desc'] = c.text
+                desc_lower = c.text.lower()
+                for t in transm:
+                    if t in desc_lower or t in title_lower:
+                        item['transm'] = t
+                        break
+                for t,s in zip(years,years_short):
+                    if t in desc_lower or t in title_lower or re.search('(^| )%s '%s, desc_lower) or re.search('(^| )%s '%s, title_lower):
+                        item['year'] = t
+                        break
+                for t in level:
+                    if t in desc_lower or t in title_lower:
+                        item['level'] = t
+                        break
+                for t in trim:
+                    if t in desc_lower or t in title_lower:
+                        item['trim'] = t
+                        break
+        items.append(item)
+
+    print len(items)
+    
+    c = csv.writer(open("civic.csv", "wb"))
+    for i in items:
+        c.writerow(i.values())
+        
+        
---
+++ src/CraigsCrawler/main2.py
@@ -0,0 +1,92 @@
+'''
+Created on Jun 13, 2013
+
+@author: yuncong
+'''
+import xml.etree.ElementTree as ET
+import urllib
+import re
+import csv
+
+prefix = 'http://sandiego.craigslist.org'
+
+# search_name = 'civic'
+# brand = 'honda'
+# model = 'civic'
+# trim = ['ex','lx','si','dx']
+
+# search_name = 'fit'
+# brand = 'honda'
+# model = 'fit'
+# trim = ['sport']
+
+search_name = 'corolla'
+brand = 'toyota'
+model = 'corolla'
+trim = ['s','le','l','dx','xle','xrs','ce']
+
+type = ['sedan','coupe','hatchback']
+years = [str(y) for y in range(1992,2014)]
+years_short = [y[-2:]for y in years]
+transm = ['automatic','manual']
+srch_year = '2006'
+start = 0
+
+if __name__ == '__main__':
+    url = 'http://sandiego.craigslist.org/search/cta?query='+brand+'%20'+model+'&srchType=T&s='+str(start)
+    xml_str = urllib.urlopen(url).read()
+    contents = re.findall('<p class="row" data-pid=".*?">(.*?)</p>', xml_str, re.M|re.S)
+    
+    keys = ['date','price','title','url','owner','trim','year','type','transm','cond']
+    items = []
+
+    for c in contents:
+        item = dict([(k,None) for k in keys])
+        
+        item['date'] = re.findall('<span class="date">(.*?)</span>', c)[0]
+        try:
+            item['price'] = re.findall('<span class="price">(.*?)</span>', c)[0]
+        except:
+            pass
+
+        item['title'] = re.findall('<a href=".*\.html">(.*?)</a> ?</span>', c)[0].strip()
+        title_lower = item['title'].lower()
+        
+        item['url'] = prefix + re.findall('<a href="(.*?)"', c)[0]
+        page = urllib.urlopen(item['url']).read()
+        try:
+            posting = re.findall('<section id="postingbody">(.*?)</section>', page, re.M|re.S)[0]
+        except:
+            continue
+        posting_lower = posting.lower()
+        
+        for t in transm:
+            if t in posting_lower or t in title_lower:
+                item['transm'] = t
+                break
+        for t,s in zip(years,years_short):
+            if re.search('(\W|^| )%s(\W|$| )'%t, posting_lower) \
+                    or re.search('(\W|^| )%s(\W|$| )'%t, title_lower)\
+                    or re.search('(\W|^| )%s(\W|$| )'%s, posting_lower) \
+                    or re.search('(\W|^| )%s(\W|$| )'%s, title_lower):
+                item['year'] = t
+                break
+        for t in type:
+            if t in posting_lower or t in title_lower:
+                item['type'] = t
+                break
+        for t in trim:
+            if re.search('(\W|^| )%s(\W|$| )'%t, posting_lower) or re.search('(\W|^| )%s(\W|$| )'%t, title_lower):
+                item['trim'] = t
+                break
+        
+        if 'salvage' in posting_lower or 'salvage' in title_lower:
+            item['cond'] = 'salvage'
+        
+        item['owner'] = re.findall('<a class="gc".*?by (.*?)</a>', c)[0]
+        items.append(item)
+    
+    dw = csv.DictWriter(open(search_name+".csv", "wb"), fieldnames=keys)
+#     dw.writerow(dict((fn,fn) for fn in keys))
+    for i in items:
+        dw.writerow(i)
